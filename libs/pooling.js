8242645f6b15865d76c767db3cf3138e816b62c5
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-08-29 16:50:38 -0500

    Uso de variables-entornos

--- libs/pooling.js
+++ libs/pooling.js
@@ -1,5 +1,12 @@
 const {Pool} = require('pg');
+const {config} = require('./../config/config');
 
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPasswUser);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+/*
   const pool = new Pool({
     host:'localhost',
     port:5432,
@@ -7,6 +14,9 @@ const {Pool} = require('pg');
     password:'r3b3c4',
     database:'my_store_',
   });
+*/
+
+const pool = new Pool({connectionString: URI});
 
 module.exports = pool;
 
